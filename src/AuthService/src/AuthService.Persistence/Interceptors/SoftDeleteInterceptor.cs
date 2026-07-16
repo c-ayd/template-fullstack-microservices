67d0ee5ef1516857c1a7ac7ea8aae9db3371b4ff
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-07-16 16:51:51 +0200

    create soft delete interceptor

---
+++ src/AuthService/src/AuthService.Persistence/Interceptors/SoftDeleteInterceptor.cs
@@ -0,0 +1,48 @@
+using AuthService.Domain.SeedWork;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace AuthService.Persistence.Interceptors
+{
+    public class SoftDeleteInterceptor : SaveChangesInterceptor
+    {
+        public override InterceptionResult<int> SavingChanges(DbContextEventData eventData, InterceptionResult<int> result)
+        {
+            CheckSoftDeletedEntities(eventData);
+            return base.SavingChanges(eventData, result);
+        }
+
+        public override ValueTask<InterceptionResult<int>> SavingChangesAsync(DbContextEventData eventData, InterceptionResult<int> result, CancellationToken cancellationToken = default)
+        {
+            CheckSoftDeletedEntities(eventData);
+            return base.SavingChangesAsync(eventData, result, cancellationToken);
+        }
+
+        private void CheckSoftDeletedEntities(DbContextEventData eventData)
+        {
+            var entriesToBeDeleted = eventData.Context?.ChangeTracker.Entries()
+                .Where(e => e.State == EntityState.Deleted)
+                .ToList();
+
+            if (entriesToBeDeleted == null || entriesToBeDeleted.Count == 0)
+                return;
+
+            foreach (var entry in entriesToBeDeleted)
+            {
+                var entityType = entry.Entity.GetType();
+                if (!entityType.IsAssignableTo(typeof(ISoftDelete)))
+                    continue;
+
+                var isDeletedProperty = entityType.GetProperty(nameof(ISoftDelete.IsDeleted))!;
+                var isDeleted = (bool)isDeletedProperty.GetValue(entry.Entity)!;
+                if (isDeleted)
+                    continue;
+
+                isDeletedProperty.SetValue(entry.Entity, true);
+                entityType.GetProperty(nameof(ISoftDelete.DeletedDate))!.SetValue(entry.Entity, DateTime.UtcNow);
+
+                entry.State = EntityState.Modified;
+            }
+        }
+    }
+}
