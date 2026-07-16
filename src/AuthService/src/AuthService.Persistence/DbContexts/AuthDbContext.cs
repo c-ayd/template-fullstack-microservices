6c073a5cbdd8ed94e87899976472b78395b2116b
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-07-16 15:25:26 +0200

    create authdbcontext

---
+++ src/AuthService/src/AuthService.Persistence/DbContexts/AuthDbContext.cs
@@ -0,0 +1,17 @@
+using AuthService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Persistence.DbContexts
+{
+    public class AuthDbContext : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Login> Logins { get; set; }
+        public DbSet<Token> Tokens { get; set; }
+
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+    }
+}
