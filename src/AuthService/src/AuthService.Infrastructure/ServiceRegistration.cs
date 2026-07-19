79d677d562e1a5acf2fcbf738ab2d1a749b80473
c-ayd <201085822+c-ayd@users.noreply.github.com>
2026-07-19 14:35:38 +0200

    register password hasher in infrastructure services

--- src/AuthService/src/AuthService.Infrastructure/ServiceRegistration.cs
+++ src/AuthService/src/AuthService.Infrastructure/ServiceRegistration.cs
@@ -1,5 +1,7 @@
 using AuthService.Application.Abstractions.Authentication;
+using AuthService.Application.Abstractions.Crypto;
 using AuthService.Infrastructure.Authentication;
+using AuthService.Infrastructure.Crypto;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AuthService.Infrastructure
@@ -10,6 +12,8 @@ namespace AuthService.Infrastructure
         {
             services.AddSingleton<IJwtKeyService, JwtKeyService>();
             services.AddSingleton<IJwtService, JwtService>();
+
+            services.AddSingleton<IPasswordHasher, Pbkdf2>();
         }
     }
 }
